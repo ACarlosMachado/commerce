7899555f17dd22e0d97c281121106183c6d06b3e
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-09-13 17:40:32 -0300

    fix: lint req 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,8 +1,8 @@
 db.produtos.find({
-    ingredientes: { $eq: "picles" }
+    ingredientes: { $eq: "picles" },
 }, {
     nome: 1,
     ingredientes: 1,
     valoresNutricionais: { $slice: 3 },
-    _id:0
+    _id: 0,
 });
