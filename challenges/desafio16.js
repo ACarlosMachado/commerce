e9d22914aad4ca6bf7ed482f0ac46443e94fee7f
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-09-13 19:37:07 -0300

    feat: req 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,12 @@
+db.produtos.update({
+    nome: "Big Mac",
+}, {
+    $set: { ultimaModificacao: { $type: "timestamp" } },
+});
+
+db.produtos.find({
+    ultimaModificacao: { $exists: true },
+}, {
+    nome: 1,
+    _id: 0,
+});
