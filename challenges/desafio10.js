c3fb6eabd977a90d381a6ae2414d0cf25f90fd52
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-09-13 17:27:16 -0300

    fix: lint req 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,10 +2,10 @@ db.produtos.find({
     valoresNutricionais: {
         $elemMatch: {
             tipo: "proteínas",
-            percentual:  {$gte: 30, $lte: 40}
-        }
-    }
+            percentual: { $gte: 30, $lte: 40 },
+        },
+    },
 }, {
     nome: 1,
-    _id:0
+    _id: 0,
 });
